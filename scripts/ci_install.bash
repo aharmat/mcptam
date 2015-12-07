3d19aea687ea5c55c55f8ada5dca7d13f198847e
Chris Choi <chutsu@gmail.com>
2015-12-07 13:53:59 -0500

    Refactor install scripts and travis.yml

--- scripts/ci_install.bash
+++ scripts/ci_install.bash
@@ -1,10 +1,5 @@
 #!/bin/bash
 set -e  # exit on first error
-ROS_PACKAGES_URL='http://packages.ros.org/ros/ubuntu'
-APT_KEYS_URL='hkp://pool.sks-keyservers.net:80'
-APT_TARGETS="$(lsb_release -sc) main"
-SOURCES_LIST_TARGET='/etc/apt/sources.list.d/ros-latest.list'
-
 TOON_VERSION="2.2"
 TOON_FORMAT="TooN-$TOON_VERSION"
 TOON_URL="http://www.edwardrosten.com/cvd/$TOON_FORMAT.tar.gz"
@@ -17,37 +12,6 @@ GVARS3_VERSION="3.0"
 GVARS3_FORMAT="gvars-$GVARS3_VERSION"
 GVARS3_URL="http://www.edwardrosten.com/cvd/$GVARS3_FORMAT.tar.gz"
 
-ROS_VERSION="indigo"
-ROS_BASH="/opt/ros/$ROS_VERSION/setup.bash"
-
-
-install_ros()
-{
-	# update sources.list and add apt-keys
-	echo "deb $ROS_PACKAGES_URL $APT_TARGETS" > $SOURCES_LIST_TARGET
-	apt-key adv --keyserver $APT_KEYS_URL --recv-key "0xB01FA116"
-
-	# update apt and install ros
-	apt-get update
-	apt-get install -y ros-$ROS_VERSION-desktop-full
-
-	# initialize rosdep
-	rosdep init
-	rosdep update
-
-	# env setup
-	echo "source /opt/ros/$ROS_VERSION/setup.bash" >> $HOME/.bashrc
-
-	# install ros
-	apt-get install -y python-rosinstall
-
-	# install ros packages	
-	apt-get install -y \
-		ros-$ROS_VERSION-pcl-ros \
-		ros-$ROS_VERSION-image-transport \
-		ros-$ROS_VERSION-image-transport-plugins \
-		ros-$ROS_VERSION-libg2o
-}
 
 install_prerequisits()
 {
